a0b529a15ca20faac018fe4e964594aa4f77a55e
Matt Porritt <mattp@catalyst-au.net>
2020-09-03 05:03:00 +0000

    fix double bounce

--- amd/src/broadcast_modal.js
+++ amd/src/broadcast_modal.js
@@ -61,18 +61,19 @@ function(Str, ModalFactory, ModalEvents, Ajax, Notification) {
      * everytime we poll, and prevent the user from being logged out due to inactivity.
      */
     var checkMessages = function() {
-        Ajax.call([{
-            methodname: 'tool_broadcast_check_broadcasts',
-            args: {contextid: contextid}
-        }], true, false)[0].done(function(response) {
-            var responseObj = JSON.parse(response);
-            if (responseObj) { // We have messages.
-                getMessages();
-            }
-        }).fail(function() {
-            window.console.error(new Error('Failed to check broadcast messages'));
-        });
-
+        if (document.hasFocus()) {
+            Ajax.call([{
+                methodname: 'tool_broadcast_check_broadcasts',
+                args: {contextid: contextid}
+            }], true, false)[0].done(function(response) {
+                var responseObj = JSON.parse(response);
+                if (responseObj) { // We have messages.
+                    getMessages();
+                }
+            }).fail(function() {
+                window.console.error(new Error('Failed to check broadcast messages'));
+            });
+        }
     };
 
     /**
@@ -129,20 +130,22 @@ function(Str, ModalFactory, ModalEvents, Ajax, Notification) {
      * Display the message to the user.
      */
     var displayMessages = function() {
-        // If modal window is not currently displayed, check for queue messages.
-        for (var message in messageQueue) {
-            if (messageQueue[message].mode == 1) { // Display the message in a modal.
-                displayMessageModal(messageQueue[message]);
-            } else if (messageQueue[message].mode == 2) { // Display the message as notification.
-                displayMessageNotification(messageQueue[message]);
-            } else if (messageQueue[message].mode == 3) { // Display the message both ways.
-                displayMessageModal(messageQueue[message]);
-                displayMessageNotification(messageQueue[message]);
-            }
+        if (document.hasFocus()) {
+            // If modal window is not currently displayed, check for queue messages.
+            for (var message in messageQueue) {
+                if (messageQueue[message].mode == 1) { // Display the message in a modal.
+                    displayMessageModal(messageQueue[message]);
+                } else if (messageQueue[message].mode == 2) { // Display the message as notification.
+                    displayMessageNotification(messageQueue[message]);
+                } else if (messageQueue[message].mode == 3) { // Display the message both ways.
+                    displayMessageModal(messageQueue[message]);
+                    displayMessageNotification(messageQueue[message]);
+                }
 
-            // Exit the loop after showing one message.
-            break;
+                // Exit the loop after showing one message.
+                break;
             }
+        }
     };
 
     /**
